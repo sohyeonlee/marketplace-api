ce3d29bfa74557221da6a7134f494e7c81a163c9
sohyeonlee <sohyeonlee@berkeley.edu>
2015-09-21 00:54:32 -0700

    User controller added

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ MarketPlaceApi::Application.routes.draw do
   constraints: {subdomain: 'api'}, path: '/' do
     scope module: :v1,
     constraints: ApiConstraints.new(version: 1, defalut: true) do
+      resources :users, :only => [:show]
     end
   end
 end
