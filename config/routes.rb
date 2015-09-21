b2f2f9935e2704214a8239eb0d3d1e8b8eb89b55
sohyeonlee <sohyeonlee@berkeley.edu>
2015-09-21 12:31:15 -0700

    user api endpoint created

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
   devise_for :users
   # Api definition
-  namespace :api, defaults: {format: :json},
-  constraints: {subdomain: 'api'}, path: '/' do
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1,
-    constraints: ApiConstraints.new(version: 1, defalut: true) do
-      resources :users, :only => [:show]
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      resources :users, :only => [:show, :create]
     end
   end
 end
